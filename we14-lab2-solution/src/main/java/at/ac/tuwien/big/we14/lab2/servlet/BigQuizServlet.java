8d74c133a65baa201e0d1fd6bedf9efc2f302930
MichaelPazourek <privat@mbpstudios.net>
2014-04-21 13:23:25 +0200

    Servlet Implementation (Kopiert von RegistratinMVC)

--- we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,3 +1,30 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 // Your Servlet implementation
+
+public class BigQuizServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+	        
+	        String action = request.getParameter("action");
+	        if(action==null) {
+	            return;
+	        }
+	        if(action.equals("login")) {     
+	        	
+	        }
+	        
+	 }
+	
+}
