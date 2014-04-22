dc285619bcc49ac5e91ec41393f9a9e9cd623fc6
MichaelPazourek <privat@mbpstudios.net>
2014-04-22 11:06:42 +0200

    Big Quiz ausgaben

--- we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -37,15 +38,71 @@ public class BigQuizServlet extends HttpServlet {
 			
 	        if(action.equals("start")) {  
 	        	
-	        	HttpSession session = request.getSession(true);//vllt nicht wichtig
+	        	
 	        	
 	        	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/question.jsp");
 	            dispatcher.forward(request, response);  
 	        	
-	        }else if (action.equals("question")){
+	        }else if (action.equals("questioncomplete")){
+	        	
+	        	
+	        }else if (action.equals("roundcompleteweiter")){
+	        	//sind 5 runden vergangen, dann kommt finish html, sonst kommen neue question.jsp seiten.
 	        	
 	        	if(true){
+	        		PrintWriter out = response.getWriter();
+	                out.println("<html>");
+	                out.println("<head>");
+	                out.println("<title>RoundComplete getriggert</title>");            
+	                out.println("</head>");
+	                out.println("<body>");
+	                out.println("<h1>Hello "  + "</h1>");
+	                out.println("</body>");
+	                out.println("</html>");
 	        		
+	        	}else{
+	        		
+	        		
+	        	}
+	        	
+	        	
+	        	
+	        	//RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/finish.jsp");
+	            //dispatcher.forward(request, response);  
+	        	//if counter == 3 -> finish.html
+	        	//else roundcomplete.html
+	        	
+	        }
+	 }
+	
+	 @Override
+	   protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+		 	
+		 String action = request.getParameter("action");
+	        
+	       
+			if(action==null) {
+	            return;
+	        }
+			
+	        if(action.equals("questioncomplete")) { 
+	        	
+	        	//sind schon 3 fragen gefragt? wenn ja counter zurücksetzen und auf roundcomplete.jsp gehen
+	        	
+	        	HttpSession session = request.getSession(true);
+	        	
+	        	
+	        	if(true){
+	        		PrintWriter out = response.getWriter();
+	                out.println("<html>");
+	                out.println("<head>");
+	                out.println("<title>Question Complete getriggert</title>");            
+	                out.println("</head>");
+	                out.println("<body>");
+	                out.println("<h1>Hello " + request.getParameter("timeleftvalue") + "</h1>");
+	                out.println("</body>");
+	                out.println("</html>");
 	        		
 	        	}else{
 	        		
@@ -55,19 +112,8 @@ public class BigQuizServlet extends HttpServlet {
 	        	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/question.jsp");
 	            dispatcher.forward(request, response);  
 	        	
-	        }else if (action.equals("roundcomplete")){
-	        	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/finish.jsp");
-	            dispatcher.forward(request, response);  
-	        	//if counter == 3 -> finish.html
-	        	//else roundcomplete.html
 	        	
 	        }
-	 }
-	
-	 @Override
-	    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-	            throws ServletException, IOException {
-
 		 
 	 }
 	
