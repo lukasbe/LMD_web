dc728e5b728cebf9a23e0607c43c59c373fe514c
Daniel <Daniel@128.131.210.214>
2014-04-22 13:05:36 +0200

    SimpleCategoryGenerator erstellt

---
+++ we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/api/impl/SimpleCategoryGenerator.java
@@ -0,0 +1,45 @@
+package at.ac.tuwien.big.we14.lab2.api.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import at.ac.tuwien.big.we14.lab2.api.Category;
+
+public class SimpleCategoryGenerator {
+
+	List<Category> alreadyUsedCategory = new ArrayList<Category>();
+	List<Category> categories = new ArrayList<Category>();
+	
+	public SimpleCategoryGenerator(){}
+	
+	public SimpleCategoryGenerator(List<Category> catergories)
+	{
+		this.categories = catergories;
+	}
+	
+	public Category getCategory()
+	{		
+		List<Category> list = new ArrayList<Category>();
+		
+		for(Category c: categories)
+		{
+			// Falls die Category noch nicht verwendet wurde
+			if(!alreadyUsedCategory.contains(c))
+			{
+				list.add(c);
+			}
+		}
+		
+		if(list.isEmpty())
+		{
+			return null;
+		}
+		
+		Random r = new Random();
+		Category c = list.get(r.nextInt(list.size()));
+		alreadyUsedCategory.add(c);
+		
+		return c;
+	}
+}
