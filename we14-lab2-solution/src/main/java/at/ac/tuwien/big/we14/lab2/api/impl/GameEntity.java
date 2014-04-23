825bbf327fabde113c7f56f73d44b91f197043b0
MichaelPazourek <privat@mbpstudios.net>
2014-04-23 15:01:21 +0200

    game entity logs

--- we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/api/impl/GameEntity.java
+++ we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/api/impl/GameEntity.java
@@ -5,10 +5,13 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import at.ac.tuwien.big.we14.lab2.api.Question;
+import at.ac.tuwien.big.we14.lab2.servlet.BigQuizServlet;
 
 public class GameEntity {
-
+	protected static Logger log = Logger.getLogger(BigQuizServlet.class);
 	// Key = CategoryName meistens 5, List<Question> = questioncount viele Fragen pro Key
 	private HashMap<String, List<Question>> Game = new HashMap<String, List<Question>>();
 	private List<String> roundList = new ArrayList<String>();
@@ -22,6 +25,7 @@ public class GameEntity {
 	}
 
 	public boolean hasNextRound(){
+		log.info("has next round aufgerufen");
 		if(roundCount<=0){
 			return false;
 		}else{
@@ -34,6 +38,7 @@ public class GameEntity {
 		Iterator<String> it = roundList.iterator();
 		while(it.hasNext())
 		{
+			log.info("nextround whileschleife passiert");
 		    String Runde = it.next();
 		    if(count == roundCount){
 		    	roundCount--;
