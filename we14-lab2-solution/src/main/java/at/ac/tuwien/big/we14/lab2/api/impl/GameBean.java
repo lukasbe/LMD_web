97b900dba300e243ecdf56d7c4a54cabc26479ec
lukasbe <luki.berger@gmail.com>
2014-04-24 13:13:15 +0200

    GameBean

---
+++ we14-lab2-solution/src/main/java/at/ac/tuwien/big/we14/lab2/api/impl/GameBean.java
@@ -0,0 +1,19 @@
+package at.ac.tuwien.big.we14.lab2.api.impl;
+
+import java.util.List;
+
+public class GameBean {
+	
+	private String player1;
+	private List<Boolean> player1RoundSummary;
+	private int player1WonRounds;
+	private String player2;
+	private List<Boolean> player2RoundSummary;
+	private int player2WonRounds;
+	private int currentround;
+	private String gameWinner;
+	private String currentroundwinner;
+	private int roundsQuantity;
+	private int questionsQuantity;
+
+}
